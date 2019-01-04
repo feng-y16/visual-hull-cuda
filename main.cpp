7494aac7cff4e13762e842f0d9a0dda66ffbc8a7
feng-y16 <46351482+feng-y16@users.noreply.github.com>
2019-01-04 10:32:10 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,347 @@
+#pragma warning(disable:4244)
+#pragma warning(disable:4267)
+#pragma warning(disable:4819)
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <fstream>
+#include <Eigen/Eigen>
+#include "main.h"
+
+
+std::vector<int> voxel(IX*IY*IZ,1);
+
+cudamodel::cudamodel(int resX, int resY, int resZ)
+{
+
+}
+
+struct Projection
+{
+	Eigen::Matrix<float, 3, 4> m_projMat;
+	cv::Mat m_image;
+	uint m_threshold;
+	Projection(){
+		m_threshold = 125;
+	}
+
+	bool outOfRange(int x, int max)
+	{
+		return x < 0 || x >= max;
+	}
+
+	bool checkRange(double x, double y, double z)
+	{
+		Eigen::Vector3f vec3 = m_projMat * Eigen::Vector4f(x, y, z, 1);
+		int indX = vec3[1] / vec3[2];
+		int indY = vec3[0] / vec3[2];
+		if (outOfRange(indX, height) || outOfRange(indY, width))
+			return false;
+		return m_image.at<uchar>(indX, indY) > m_threshold;
+	}
+};
+
+// 用于index和实际坐标之间的转换
+struct CoordinateInfo
+{
+	int m_resolution;
+	double m_min;
+	double m_max;
+
+	double index2coor(int index)
+	{
+		return m_min + index * (m_max - m_min) / m_resolution;
+	}
+
+	CoordinateInfo(int resolution = 10, double min = 0.0, double max = 100.0)
+		: m_resolution(resolution)
+		, m_min(min)
+		, m_max(max)
+	{
+	}
+};
+
+class Model
+{
+public:
+	typedef std::vector<std::vector<bool>> Pixel;
+	typedef std::vector<Pixel> Voxel;
+
+	Model(int resX = 100, int resY = 100, int resZ = 100);
+	~Model();
+
+	void saveModel(const char* pFileName);
+	void saveModelWithNormal(const char* pFileName);
+	void loadMatrix(const char* pFileName);
+	void loadImage(const char* pDir, const char* pPrefix, const char* pSuffix);
+	void getModel();
+	void getSurface();
+	Eigen::Vector3f getNormal(int indX, int indY, int indZ);
+
+private:
+	CoordinateInfo m_corrX;
+	CoordinateInfo m_corrY;
+	CoordinateInfo m_corrZ;
+
+	int m_neiborSize;
+
+	std::vector<Projection> m_projectionList;
+	Voxel m_surface;
+};
+
+Model::Model(int resX, int resY, int resZ)
+	: m_corrX(resX, -5, 5)
+	, m_corrY(resY, -10, 10)
+	, m_corrZ(resZ, 15, 30)
+{
+	if (resX > 100)
+		m_neiborSize = resX / 100;
+	else
+		m_neiborSize = 1;
+	m_surface = Voxel(m_corrX.m_resolution, Pixel(m_corrY.m_resolution, std::vector<bool>(m_corrZ.m_resolution, true)));
+}
+
+Model::~Model()
+{
+}
+
+void Model::saveModel(const char* pFileName)
+{
+	std::ofstream fout(pFileName);
+
+	for (int indexX = 0; indexX < m_corrX.m_resolution; indexX++)
+		for (int indexY = 0; indexY < m_corrY.m_resolution; indexY++)
+			for (int indexZ = 0; indexZ < m_corrZ.m_resolution; indexZ++)
+				if (m_surface[indexX][indexY][indexZ])
+				{
+					double coorX = m_corrX.index2coor(indexX);
+					double coorY = m_corrY.index2coor(indexY);
+					double coorZ = m_corrZ.index2coor(indexZ);
+					fout << coorX << ' ' << coorY << ' ' << coorZ << std::endl;
+				}
+}
+
+void Model::saveModelWithNormal(const char* pFileName)
+{
+	std::ofstream fout(pFileName);
+
+	double midX = m_corrX.index2coor(m_corrX.m_resolution / 2);
+	double midY = m_corrY.index2coor(m_corrY.m_resolution / 2);
+	double midZ = m_corrZ.index2coor(m_corrZ.m_resolution / 2);
+
+	for (int indexX = 0; indexX < m_corrX.m_resolution; indexX++)
+		for (int indexY = 0; indexY < m_corrY.m_resolution; indexY++)
+			for (int indexZ = 0; indexZ < m_corrZ.m_resolution; indexZ++)
+				if (m_surface[indexX][indexY][indexZ])
+				{
+					double coorX = m_corrX.index2coor(indexX);
+					double coorY = m_corrY.index2coor(indexY);
+					double coorZ = m_corrZ.index2coor(indexZ);
+					fout << coorX << ' ' << coorY << ' ' << coorZ << ' ';
+
+					Eigen::Vector3f nor = getNormal(indexX, indexY, indexZ);
+					fout << nor(0) << ' ' << nor(1) << ' ' << nor(2) << std::endl;
+				}
+}
+
+void Model::loadMatrix(const char* pFileName)
+{
+	std::ifstream fin(pFileName);
+
+	int num;
+	Eigen::Matrix<float, 3, 3> matInt;
+	Eigen::Matrix<float, 3, 4> matExt;
+	Projection projection;
+	while (fin >> num)
+	{
+		for (int i = 0; i < 3; i++)
+			for (int j = 0; j < 3; j++)
+				fin >> matInt(i, j);
+
+		double temp;
+		fin >> temp;
+		fin >> temp;
+		for (int i = 0; i < 3; i++)
+			for (int j = 0; j < 4; j++)
+				fin >> matExt(i, j);
+
+		projection.m_projMat = matInt * matExt;
+		m_projectionList.push_back(projection);
+	}
+}
+
+void Model::loadImage(const char* pDir, const char* pPrefix, const char* pSuffix)
+{
+	int fileCount = m_projectionList.size();
+	std::string fileName(pDir);
+	fileName += '/';
+	fileName += pPrefix;
+	for (int i = 0; i < fileCount; i++)
+	{
+		std::cout << fileName + std::to_string(i) + pSuffix << std::endl;
+		m_projectionList[i].m_image = cv::imread(fileName + std::to_string(i) + pSuffix, CV_8UC1);
+	}
+}
+
+void Model::getModel()
+{
+	cudamodel cumodel(300, 300, 300);
+	float m1[3][4];
+	uchar* mat[20];
+	std::vector<uchar> m2(prejectionCount*height*width);
+	for(int n=0;n<prejectionCount;n++)
+	{
+		for(int i=0;i<3;i++)
+		{
+			for(int j=0;j<4;j++)
+			{
+			    m1[i][j]=m_projectionList[n].m_projMat(i,j);
+			}
+		}
+		for(int i=0;i<height;i++)
+		{
+			for(int j=0;j<width;j++)
+			{
+			    m2[n*width*height+i*width+j]=m_projectionList[n].m_image.at<uchar>(i,j);
+			}
+		}
+		cumodel.m_projectionList.push_back(cudaProjection(m1));
+		mat[n]=m2.data()+n*width*height;
+	}
+	cumodel.compute(voxel.data(),cumodel.m_projectionList.data(),mat);
+}
+
+void Model::getSurface()
+{
+	// 邻域：上、下、左、右、前、后。
+	int dx[6] = { -1, 0, 0, 0, 0, 1 };
+	int dy[6] = { 0, 1, -1, 0, 0, 0 };
+	int dz[6] = { 0, 0, 0, 1, -1, 0 };
+
+	// lambda表达式，用于判断某个点是否在Voxel的范围内
+	auto outOfRange = [&](int indexX, int indexY, int indexZ){
+		return indexX < 0 || indexY < 0 || indexZ < 0
+			|| indexX >= m_corrX.m_resolution
+			|| indexY >= m_corrY.m_resolution
+			|| indexZ >= m_corrZ.m_resolution;
+	};
+	int ix=0;
+	int iy=0;
+	int iz=0;
+	int i=0;
+	for (int indexX = 0; indexX < m_corrX.m_resolution; indexX++)
+		for (int indexY = 0; indexY < m_corrY.m_resolution; indexY++)
+			for (int indexZ = 0; indexZ < m_corrZ.m_resolution; indexZ++)
+			{
+				if (!voxel[indexX*IY*IZ+indexY*IZ+indexZ])
+				{
+					m_surface[indexX][indexY][indexZ] = false;
+					continue;
+				}
+				bool ans = false;
+				i=0;
+				while(!ans&&i<6)
+				{
+					ix=indexX + dx[i];
+					iy=indexY + dy[i];
+					iz=indexZ + dz[i];
+					ans = ans || outOfRange(ix, iy, iz)|| !voxel[ix*IY*IZ+iy*IZ+iz];
+					++i;
+				}
+				m_surface[indexX][indexY][indexZ] = ans;
+			}
+}
+
+Eigen::Vector3f Model::getNormal(int indX, int indY, int indZ)
+{
+	auto outOfRange = [&](int indexX, int indexY, int indexZ){
+		return indexX < 0 || indexY < 0 || indexZ < 0
+			|| indexX >= m_corrX.m_resolution
+			|| indexY >= m_corrY.m_resolution
+			|| indexZ >= m_corrZ.m_resolution;
+	};
+
+	std::vector<Eigen::Vector3f> neiborList;
+	std::vector<Eigen::Vector3f> innerList;
+
+	for (int dX = -m_neiborSize; dX <= m_neiborSize; dX++)
+		for (int dY = -m_neiborSize; dY <= m_neiborSize; dY++)
+			for (int dZ = -m_neiborSize; dZ <= m_neiborSize; dZ++)
+			{
+				if (!dX && !dY && !dZ)
+					continue;
+				int neiborX = indX + dX;
+				int neiborY = indY + dY;
+				int neiborZ = indZ + dZ;
+				if (!outOfRange(neiborX, neiborY, neiborZ))
+				{
+					float coorX = m_corrX.index2coor(neiborX);
+					float coorY = m_corrY.index2coor(neiborY);
+					float coorZ = m_corrZ.index2coor(neiborZ);
+					if (m_surface[neiborX][neiborY][neiborZ])
+						neiborList.push_back(Eigen::Vector3f(coorX, coorY, coorZ));
+					else if (voxel[neiborX*IY*IZ+neiborY*IZ+neiborZ])
+						innerList.push_back(Eigen::Vector3f(coorX, coorY, coorZ));
+				}
+			}
+
+	Eigen::Vector3f point(m_corrX.index2coor(indX), m_corrY.index2coor(indY), m_corrZ.index2coor(indZ));
+
+	Eigen::MatrixXf matA(3, neiborList.size());
+	for (int i = 0; i < neiborList.size(); i++)
+		matA.col(i) = neiborList[i] - point;
+	Eigen::SelfAdjointEigenSolver<Eigen::Matrix3f> eigenSolver(matA * matA.transpose());
+	Eigen::Vector3f eigenValues = eigenSolver.eigenvalues();
+	int indexEigen = 0;
+	if (abs(eigenValues[1]) < abs(eigenValues[indexEigen]))
+		indexEigen = 1;
+	if (abs(eigenValues[2]) < abs(eigenValues[indexEigen]))
+		indexEigen = 2;
+	Eigen::Vector3f normalVector = eigenSolver.eigenvectors().col(indexEigen);
+	
+	Eigen::Vector3f innerCenter = Eigen::Vector3f::Zero();
+	for (auto const& vec : innerList)
+		innerCenter += vec;
+	innerCenter /= innerList.size();
+
+	if (normalVector.dot(point - innerCenter) < 0)
+		normalVector *= -1;
+	return normalVector;
+}
+
+int main(int argc, char** argv)
+{
+	clock_t t = clock();
+
+	// 分别设置xyz方向的Voxel分辨率
+	Model model(300, 300, 300);
+
+	// 读取相机的内外参数
+	model.loadMatrix("../../calibParamsI.txt");
+	
+	// 读取投影图片
+	model.loadImage("../../wd_segmented", "WD2_", "_00020_segmented.png");
+
+	// 得到Voxel模型
+	model.getModel();
+	std::cout << "get model time: " << (float(clock()-t) / CLOCKS_PER_SEC) << "seconds\n";
+	std::cout << "get model done\n";
+
+	// 获得Voxel模型的表面
+	model.getSurface();
+	std::cout << "get surface done\n";
+
+	// 将模型导出为xyz格式
+	model.saveModel("../../WithoutNormal.xyz");
+	std::cout << "save without normal done\n";
+
+	model.saveModelWithNormal("../../WithNormal.xyz");
+	std::cout << "save with normal done\n";
+
+	system("PoissonRecon.x64 --in ../../WithNormal.xyz --out ../../mesh.ply");
+	std::cout << "save mesh.ply done\n";
+
+	t = clock() - t;
+	std::cout << "time: " << (float(t) / CLOCKS_PER_SEC) << "seconds\n";
+
+	return 0;
+}
